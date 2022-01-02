d3fbafcef47f7830635f1349faace840291e2647
pcrespo2017 <37112566+pcrespo2017@users.noreply.github.com>
2022-01-02 23:13:32 +0100

    version 2

---
+++ Sonified_Tumor_Resection-v2/Assets/scripts/CubeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    public GameObject moving_object;
+    public GameObject cube;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(moving_object.transform.position, cube.transform.position);
+        Debug.Log(distance);
+        
+    }
+}
---
+++ Sonified_Tumor_Resection-v2/Assets/scripts/FOLLOW.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOLLOW : MonoBehaviour
+{
+
+    public Transform targ;
+    Vector3 direction;
+    public float speeed = 4.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        direction = (targ.position - transform.position).normalized;
+        if ((targ.position - transform.position).magnitude > 0.1)
+            transform.Translate(direction * Time.deltaTime * speeed);
+
+    }
+}
