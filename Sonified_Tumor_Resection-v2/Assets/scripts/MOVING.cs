7106d34e235c24f17c308a2fdbd26e46f8a62731
Marta <48037981+marteczkah@users.noreply.github.com>
2022-02-14 22:34:27 +0100

    songs change based on the distance

--- Sonified_Tumor_Resection-v2/Assets/scripts/MOVING.cs
+++ Sonified_Tumor_Resection-v2/Assets/scripts/MOVING.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class MOVING : MonoBehaviour
 
 {
@@ -11,15 +12,26 @@ public class MOVING : MonoBehaviour
     public float distance;
     public float time_ = 0f;
     Renderer rend;
+    private AudioSource audioSource;
+
+    public AudioClip song1;
+    public AudioClip song2; 
+    public int startingPitch = 4;
+
+    private bool paused1;
+    private bool paused2;
 
     // Start is called before the first frame update
     void Start()
     {
         rend = letter.GetComponent<Renderer>();
+        audioSource = GetComponent<AudioSource>();
+        paused1 = true;
+        paused2 = true;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
         float horizontal = Input.GetAxis("Horizontal");
@@ -35,32 +47,21 @@ public class MOVING : MonoBehaviour
 
         print("d" + distance);
 
-        if (distance > 4.5f)
-        {
-
-            if (Time.time - time_ >= 1.5f)
-            {
-                GetComponent<ChuckSubInstance>().RunCode(@"
-	        SinOsc foo => dac;
-		        300 => foo.freq;
-		        300::ms => now;
-            ");
-                time_ = Time.time;
-            }
+        if (paused2 && distance < 10.5) {
+            audioSource.Pause();
+            audioSource.clip = song2;
+            // audioSource.pitch = 0.5f;
+            audioSource.Play();
+            paused2 = false;
+            paused1 = true;
         }
 
-        else
-        {
-                if (Time.time - time_ >= 1)
-                {
-                    GetComponent<ChuckSubInstance>().RunCode(@"
-	        SinOsc foo => dac;
-		        600 => foo.freq;
-		        150::ms => now;
-            ");
-                    time_ = Time.time;
-                }
-
+        if (paused1 && distance >= 10.5) {
+            audioSource.Pause();
+            audioSource.clip = song1;
+            audioSource.Play();
+            paused1 = false;
+            paused2 = true;
         }
 
     }
