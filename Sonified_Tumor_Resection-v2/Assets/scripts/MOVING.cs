d7484fc4fa317650b7b9ca678de2718ac195cae8
Marta <48037981+marteczkah@users.noreply.github.com>
2022-02-15 23:16:05 +0100

    bach playing in the safe range

--- Sonified_Tumor_Resection-v2/Assets/scripts/MOVING.cs
+++ Sonified_Tumor_Resection-v2/Assets/scripts/MOVING.cs
@@ -12,15 +12,23 @@ public class MOVING : MonoBehaviour
     public float time_ = 0f;
     public int position = 1;
     public int song_samples = 0; 
-    
+    public AudioSource[] audioSource;
+    public AudioSource chuckAudio;
+    public AudioSource bachAudio;
+    public AudioClip song1;
     private int[] frequency = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     Renderer rend;
+    bool playing = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rend = letter.GetComponent<Renderer>();
-        InvokeRepeating("DistanceUpdate", (float) 0.5, (float) 0.5);
+        audioSource = GetComponents<AudioSource>();
+        bachAudio = audioSource[1];
+        bachAudio.clip = song1;
+        bachAudio.time = 5;
+        InvokeRepeating("ChuckBach", (float) 0.5, (float) 0.5);
     }
 
     void Update()
@@ -41,7 +49,7 @@ public class MOVING : MonoBehaviour
 
     // FixedUpdate is called every 0.2 seconds
     // 882 samples per 0.2 seconds 
-    void DistanceUpdate()
+    void PlainChuck()
     {
         int current_freq = 0;
         if (distance > 30) {
@@ -86,77 +94,70 @@ public class MOVING : MonoBehaviour
             1 => viol.noteOff;
             0.5 :: second => now;
         ", current_freq));
-
-        // GetComponent<ChuckSubInstance>().RunCode(string.Format (@"
-        //     // Gain g;
-        //     // g => dac;
-	    //     // SawOsc env;
-        //     // 0.0 => env.width;
-        //     // 1.2 => env.freq;
-        //     // env => g;
-
-        //     SndBuf buf;
-        //     me.sourceDir() + ""/Songs_Wav/do_i_wanna_know.wav"" => string music_file;               
-        //     music_file => buf.read; 
-        //     1.0 => buf.rate;
-        //     {0} => buf.pos; 
-        //     buf => dac;
-
-        //     chout <= ""Hello World"" <= IO.newline();
-
-        //     20::ms => now;
-        // ", position));
-
-        // if (distance > 4.5f)
-        // {
-        //     GetComponent<ChuckSubInstance>().RunCode(string.Format (@"
-        //         // Gain g;
-        //         // g => dac;
-
-	    //         // SawOsc env;
-        //         // 0.0 => env.width;
-        //         // 1.2 => env.freq;
-        //         // env => g;
-
-        //         SndBuf buf;
-        //         me.sourceDir() + ""/Songs_Wav/do_i_wanna_know.wav"" => string music_file;
-        //         music_file => buf.read; 
-        //         1.0 => buf.rate;
-        //         {0} => buf.pos; 
-        //         buf => dac;
-
-        //         chout <= ""Hello World"" <= IO.newline();
-
-        //         20::ms => now;
-        //     ", position));
-        // }
-
-        // else
-        // {
-        //     GetComponent<ChuckSubInstance>().RunCode(string.Format (@"
-        //         // Gain g;
-        //         // g => dac;
-
-        //         // SawOsc env;
-        //         // 0.0 => env.width;
-        //         // 1.2 => env.freq;
-        //         // env => g;
-
-        //         SndBuf buf_2;
-        //         me.sourceDir() + ""/Songs_Wav/do_i_wanna_know.wav"" => string music_file;
-        //         music_file => buf_2.read; 
-        //         0.2 => buf_2.rate;
-        //         {0} => buf_2.pos; 
-        //         buf_2 => dac;
-
-        //         20::ms => now;
-        //         ", position));
-        // }
-        position += 882;
-        if (position >= 12014208) {
-            position = 1;
-        }
-
     }
 
+    void ChuckBach() 
+    {
+        int current_freq = 0;
+        bool play = false;
+        if (distance > 30) {
+            play = false;
+            current_freq = 0;
+            print("f" + current_freq);
+        } else if (distance <=30 && distance > 25) {
+            play = false;
+            current_freq = 1;
+            print("f" + current_freq);
+        } else if (distance <=25 && distance > 20) {
+            play = false;
+            current_freq = 2;
+            print("f" + current_freq);
+        } else if (distance <=20 && distance > 17) {
+            play = false;
+            current_freq = 3;
+            print("f" + current_freq);
+        } else if (distance <=17 && distance > 15) {
+            play = false;
+            current_freq = 4;
+            print("f" + current_freq);
+        } else if (distance <=15 && distance > 10) {
+            play = false;
+            current_freq = 6;
+            print("f" + current_freq);
+        } else if (distance <=10 && distance > 7) {
+            play = false;
+            current_freq = 7;
+            print("f" + current_freq);
+        } else if (distance <=7 && distance > 5) {
+            play = false;
+            current_freq = 8;
+            print("f" + current_freq);
+        } else if (distance <=5 && distance > 3) {
+            play = false;
+            current_freq = 9;
+            print("f" + current_freq);
+        } else {
+            play = true;
+        }
+
+        if (play) {
+            if (playing == false) {
+                bachAudio.Play();
+                playing = true;
+            }
+        } else {
+            playing = false;
+            bachAudio.Pause();
+            GetComponent<ChuckSubInstance>().RunCode( string.Format ( @"
+                ModalBar viol => dac;
+                [10, 20, 30, 40, 50, 60, 70, 80, 90, 100] @=> int scale[];
+                Std.mtof(scale[{0}]) => viol.freq;
+                6 => viol.preset;
+                1 => viol.noteOn;
+                0.5 :: second => now;
+                1 => viol.noteOff;
+                0.5 :: second => now;
+            ", current_freq));
+        }
+    }
 }
