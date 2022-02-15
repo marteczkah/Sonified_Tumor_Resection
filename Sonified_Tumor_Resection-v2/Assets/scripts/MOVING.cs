d0e11ab0deef02e0311b26b7dd938bda6f55e787
Marta <48037981+marteczkah@users.noreply.github.com>
2022-02-15 22:19:56 +0100

    sound changes based on distance

--- Sonified_Tumor_Resection-v2/Assets/scripts/MOVING.cs
+++ Sonified_Tumor_Resection-v2/Assets/scripts/MOVING.cs
@@ -12,19 +12,19 @@ public class MOVING : MonoBehaviour
     public float time_ = 0f;
     public int position = 1;
     public int song_samples = 0; 
+    
+    private int[] frequency = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
         rend = letter.GetComponent<Renderer>();
+        InvokeRepeating("DistanceUpdate", (float) 0.5, (float) 0.5);
     }
 
-    // FixedUpdate is called every 0.2 seconds
-    // 882 samples per 0.2 seconds 
     void Update()
     {
-
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
@@ -37,11 +37,55 @@ public class MOVING : MonoBehaviour
 
         // print(rend.bounds);
         distance = (letter.position - transform.position).magnitude;
+    }
 
-        GetComponent<ChuckSubInstance>().RunCode(string.Format (@"
-            SinOsc s => dac;
-            {0}::second => now;
-        ", Time.deltaTime));
+    // FixedUpdate is called every 0.2 seconds
+    // 882 samples per 0.2 seconds 
+    void DistanceUpdate()
+    {
+        int current_freq = 0;
+        if (distance > 30) {
+            current_freq = 0;
+            print("f" + current_freq);
+        } else if (distance <=30 && distance > 25) {
+            current_freq = 1;
+            print("f" + current_freq);
+        } else if (distance <=25 && distance > 20) {
+            current_freq = 2;
+            print("f" + current_freq);
+        } else if (distance <=20 && distance > 17) {
+            current_freq = 3;
+            print("f" + current_freq);
+        } else if (distance <=17 && distance > 15) {
+            current_freq = 4;
+            print("f" + current_freq);
+        } else if (distance <=15 && distance > 10) {
+            current_freq = 5;
+            print("f" + current_freq);
+        } else if (distance <=10 && distance > 7) {
+            current_freq = 6;
+            print("f" + current_freq);
+        } else if (distance <=7 && distance > 5) {
+            current_freq = 7;
+            print("f" + current_freq);
+        } else if (distance <=5 && distance > 3) {
+            current_freq = 8;
+            print("f" + current_freq);
+        } else {
+            current_freq = 9;
+            print("f" + current_freq);
+        }
+
+        GetComponent<ChuckSubInstance>().RunCode( string.Format ( @"
+            ModalBar viol => dac;
+            [10, 20, 30, 40, 50, 60, 70, 80, 90, 100] @=> int scale[];
+            Std.mtof(scale[{0}]) => viol.freq;
+            6 => viol.preset;
+            1 => viol.noteOn;
+            0.5 :: second => now;
+            1 => viol.noteOff;
+            0.5 :: second => now;
+        ", current_freq));
 
         // GetComponent<ChuckSubInstance>().RunCode(string.Format (@"
         //     // Gain g;
