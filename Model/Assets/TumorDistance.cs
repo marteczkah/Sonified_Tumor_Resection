11229f783d4859f005f794c6b2b9dcb3e743851d
Marta <48037981+marteczkah@users.noreply.github.com>
2022-02-17 16:26:18 +0100

    connected sounds with the model

---
+++ Model/Assets/TumorDistance.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TumorDistance : MonoBehaviour
+{
+    private float distance;
+    public MeshCollider tumor;
+    public GameObject scalpelTip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Chuck", (float) 0.5, (float) 0.5);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 closestPoint = tumor.ClosestPoint(scalpelTip.transform.position);
+        distance = Vector3.Distance(closestPoint, scalpelTip.transform.position);
+    }
+
+    void Chuck()
+    {
+        int current_freq = 0;
+        float current_dist = distance * 100.0f;
+        print("scalpel - tumor: " + current_dist + "cm");
+        if (current_dist > 5.5f) { // out
+            current_freq = 0;
+        } else if (current_dist <= 5.5f && current_dist > 4.875f) { //1st outer
+            current_freq = 1;
+        } else if (current_dist <= 4.875f && current_dist > 4.250f) { //2nd outer
+            current_freq = 2;
+        } else if (current_dist <= 4.250f && current_dist > 3.625f) { //3rd outer
+            current_freq = 3;
+        } else if (current_dist <= 4.250f && current_dist > 3.625f) { //4th outer
+            current_freq = 3;
+        } else if (current_dist <= 3.625f && current_dist > 2.325f) { // cut here area
+            current_freq = 4;
+        } else if (current_dist <= 2.325f && current_dist > 2.025f) { // close to the dangerous area
+            current_freq = 5;
+        } else if (current_dist <= 2.025f) { // you too close
+            current_freq = 6;
+        } 
+        GetComponent<ChuckSubInstance>().RunCode( string.Format ( @"
+            ModalBar viol => NRev a => dac;
+            [10, 30, 40, 60, 70, 90, 100] @=> int scale[];
+            1  => a.gain;
+            0.3 => a.mix;
+            Std.mtof(scale[{0}]) => viol.freq;
+            6 => viol.preset;
+            1 => viol.noteOn;
+            0.25 :: second => now;
+            1 => viol.noteOff;
+            0.25 :: second => now;
+        ", current_freq));
+    }
+}
---
+++ Model/Assets/VesselSound.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VesselSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject scalpel;
+    public MeshCollider vesselSurface;
+    private AudioSource audioSource;
+    private float distance;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        InvokeRepeating("Vessel", (float) 0.3, (float) 0.3);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 closestPoint = vesselSurface.ClosestPoint(scalpel.transform.position);
+        distance = Vector3.Distance(closestPoint, scalpel.transform.position);
+        print("vessel - scalpel: " + distance);
+    }
+
+    // Update is called once per frame
+    void Vessel()
+    {
+        bool is_playing = false;
+        float current_dist = distance * 100.0f;
+        if (current_dist <= 1.5f && current_dist > 1.0f) {
+            audioSource.volume = 0.3f;
+            is_playing = true;
+        } else if (current_dist <=1.0f && current_dist > 0.5f) {
+            audioSource.volume = 0.6f;
+            is_playing = true;
+        } else if (current_dist <=  0.5f && current_dist > 0.0f) {
+            audioSource.volume = 1.0f;
+            is_playing = true;
+        }
+        if (is_playing) {
+            GetComponent<ChuckSubInstance>().RunCode(@"
+                Noise n => LPF f => dac;
+                // set biquad pole radius
+                //.99 => f.prad;
+                // set biquad gain
+                //100 => f.freq;
+                // set equal zeros 
+                //1 => f.eqzs;
+                // our float
+                0.0 => float t;
+
+                // infinite time-loop
+                for (0 => int i; i < 3; i++)
+                {
+                    // sweep the filter resonant frequency
+                    500.0 + Std.fabs(Math.sin(t)) * 200.0 => f.freq;
+                    t + .02 => t;
+                    // advance time
+                    0.1 :: second => now;
+                }
+            ");
+        }
+        
+    }
+}
