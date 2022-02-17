04fb33d481b034e54e1da81fce009f240534cf3f
Dilara Karaoğlu <56505156+dilarakaraoglu@users.noreply.github.com>
2022-02-17 13:33:22 +0100

    add model

---
+++ Model/Assets/RotateScalpel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScalpel : MonoBehaviour
+{
+
+    public KeyCode pressUp;
+    public KeyCode pressDown;
+    public KeyCode pressLeft;
+    public KeyCode pressRight;
+    public KeyCode pressForward;
+    public KeyCode pressBackward;
+
+
+    private float velocity = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKey(pressLeft))
+        {
+            transform.Rotate(0.0f, -5 * velocity * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey(pressRight))
+        {
+            transform.Rotate(0.0f, 5  * velocity * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey(pressForward))
+        {
+            transform.Rotate(5 * velocity * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(pressBackward))
+        {
+            transform.Rotate(-5 * velocity * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(pressUp))
+        {
+            transform.Rotate(0.0f, 0.0f, 5 * velocity * Time.deltaTime);
+        }
+        if (Input.GetKey(pressDown))
+        {
+            transform.Rotate(0.0f, 0.0f, -5 * velocity * Time.deltaTime);
+        }  
+    }
+}
---
+++ Model/Assets/ScalpelCollider.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScalpelCollider : MonoBehaviour
+{
+    public GameObject Tumor;
+    public GameObject InnerLayer;
+    public GameObject ResectionArea;
+    public GameObject BloodVessel;
+    public GameObject ScalpelTip;
+    public Text TumorDistance;
+    public Text VesselDistance;
+    public Text CurrentText;
+
+    List<float> distancesTumor = new List<float>();
+    List<float> distancesVessel = new List<float>();
+    
+
+
+    
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //tumor = GameObject.Find("Tumor");
+        //scalpelTip = GameObject.Find("ScalpelTip");
+        //innerLayer = GameObject.Find("InnerLayer");
+        //resectionArea = GameObject.Find("ResectionArea");
+        //bloodVessel = GameObject.Find("BloodVessel");
+        //tumorDistance.text = "";
+        //currentText.text = "";   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     
+}
