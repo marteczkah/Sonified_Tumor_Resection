91af01feb5f12e10f665461b1e4dae7c57198ab6
pcrespo2017 <37112566+pcrespo2017@users.noreply.github.com>
2022-02-20 18:07:18 +0100

    .
    .

---
+++ Model/Assets/MoveScalpelWithKeys.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScalpelWithKeys : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 1;
+
+    //y axis
+    public KeyCode pressUp; 
+    public KeyCode pressDown;
+    //x axis
+    public KeyCode pressLeft;
+    public KeyCode pressRight;
+    //z axis
+    public KeyCode pressBackward;
+    public KeyCode pressForward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(pressUp))
+        {
+            Vector3 objectPosition = transform.position;
+            objectPosition.y += 0.01f * speed;
+            transform.position = objectPosition;
+        }
+        if (Input.GetKey(pressDown))
+        {
+            Vector3 objectPosition = transform.position;
+            objectPosition.y -= 0.01f * speed;
+            transform.position = objectPosition;
+        }
+        if (Input.GetKey(pressLeft))
+        {
+            Vector3 objectPosition = transform.position;
+            objectPosition.x -= 0.01f * speed;
+            transform.position = objectPosition;
+        }
+        if (Input.GetKey(pressRight))
+        {
+            Vector3 objectPosition = transform.position;
+            objectPosition.x += 0.01f * speed;
+            transform.position = objectPosition;
+        }
+        if (Input.GetKey(pressForward))
+        {
+            Vector3 objectPosition = transform.position;
+            objectPosition.z -= 0.01f * speed;
+            transform.position = objectPosition;
+        }
+        if (Input.GetKey(pressBackward))
+        {
+            Vector3 objectPosition = transform.position;
+            objectPosition.z += 0.01f * speed;
+            transform.position = objectPosition;
+        }
+    }
+}
---
+++ Model/Assets/ValidationPoints.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ValidationPoints : MonoBehaviour
+{
+    public float scored_points = 0;
+    public MeshCollider tumor;
+    public GameObject scalpelTip;
+    private float tumor_distance;
+    private float too_close_time = 0;
+    bool too_close = false;
+    public TextMeshPro scoreText;
+    void Start()
+    {
+        // scoreText = GetComponent<TextMeshPro>();
+    }
+    void FixedUpdate()
+    {
+        Vector3 closestPoint = tumor.ClosestPoint(scalpelTip.transform.position);
+        tumor_distance = Vector3.Distance(closestPoint, scalpelTip.transform.position);
+    }
+    void OnTriggerEnter(Collider other) {
+        float current_dist = tumor_distance * 100.0f;
+        float initial_score = scored_points;
+        if (other.gameObject.CompareTag("YellowBigBall")) {
+            other.gameObject.SetActive(false);
+            scored_points += 0.5f;
+            too_close = false;
+        }
+        if (other.gameObject.CompareTag("YellowSmallBall")) {
+            other.gameObject.SetActive(false);
+            scored_points += 1.0f;
+            too_close = false;
+        }
+        if (other.gameObject.CompareTag("GreenBigBall")) {
+            other.gameObject.SetActive(false);
+            scored_points += 2.0f;
+            too_close = false;
+        }
+        if (other.gameObject.CompareTag("GreenSmallBall")) {
+            other.gameObject.SetActive(false);
+            scored_points += 4.0f;
+            too_close = false;
+        }
+        if (current_dist < 2.025f && current_dist > 0.5f) {
+            scored_points -= 1.0f;  
+        }
+        if (current_dist < 0.5f) {
+            scored_points = 0.0f;
+        }
+        scoreText.text = "Your score: " + scored_points;
+        print(scored_points);
+    }
+}
