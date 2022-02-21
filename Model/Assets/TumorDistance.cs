42bf1ca40d3a1f70b7d907431b7b92fb9d5f9801
Marta <48037981+marteczkah@users.noreply.github.com>
2022-02-21 21:09:39 +0100

    osc working

--- Model/Assets/TumorDistance.cs
+++ Model/Assets/TumorDistance.cs
@@ -27,63 +27,48 @@ public class TumorDistance : MonoBehaviour
 
     void Chuck()
     {
-        int current_freq = 0;
-        bool inTheArea = false;
-        bool isNearRed = false;
-        bool isInRead = false;
-        bool isTumor = false;
         float current_dist = distance * 100.0f;
         print("scalpel - tumor: " + current_dist + "cm");
         if (current_dist <= 5.5f && current_dist > 4.875f) { //1st outer
-            current_freq = 1;
-            inTheArea = true;
             var message = new OSCMessage("/message/address");
             message.AddValue(OSCValue.String("outerLayerOne"));
             transmitter.Send(message); 
         } else if (current_dist <= 4.875f && current_dist > 4.250f) { //2nd outer
-            current_freq = 2;
-            inTheArea = true;
             var message = new OSCMessage("/message/address");
             message.AddValue(OSCValue.String("outerLayerTwo"));
             transmitter.Send(message); 
         } else if (current_dist <= 4.250f && current_dist > 3.625f) { //3rd outer
-            current_freq = 3;
-            inTheArea = true;
             var message = new OSCMessage("/message/address");
             message.AddValue(OSCValue.String("outerLayerThree"));
             transmitter.Send(message); 
         } else if (current_dist <= 4.250f && current_dist > 3.625f) { //4th outer
-            current_freq = 3;
-            inTheArea = true;
             var message = new OSCMessage("/message/address");
             message.AddValue(OSCValue.String("outerLayerFour"));
             transmitter.Send(message); 
         } else if (current_dist <= 3.625f && current_dist > 2.325f) { // cut here area
-            current_freq = 4;
-            inTheArea = true;
             var message = new OSCMessage("/message/address");
             message.AddValue(OSCValue.String("inTheArea"));
             transmitter.Send(message); 
-        } else if (current_dist <= 2.325f && current_dist > 2.025f) { // close to the dangerous area
-            isNearRed = true;
+        } else if (current_dist <= 2.325f && current_dist > 2.225f) { // close to the dangerous area
             var message = new OSCMessage("/message/address");
-            message.AddValue(OSCValue.String("isNearRed"));
+            message.AddValue(OSCValue.String("isNearRed1"));
+            transmitter.Send(message); 
+        } else if (current_dist <= 2.225f && current_dist > 2.125f) { // close to the dangerous area
+            var message = new OSCMessage("/message/address");
+            message.AddValue(OSCValue.String("isNearRed2"));
+            transmitter.Send(message); 
+        } else if (current_dist <= 2.125f && current_dist > 2.025f) { // close to the dangerous area
+            var message = new OSCMessage("/message/address");
+            message.AddValue(OSCValue.String("isNearRed3"));
             transmitter.Send(message); 
         } else if (current_dist <= 2.025f &&  current_dist > 0.5f) { // you too close
-            isInRead = true;
             var message = new OSCMessage("/message/address");
             message.AddValue(OSCValue.String("isInRed"));
             transmitter.Send(message); 
         } else if (current_dist <= 0.5f && current_dist >= 0) {
-            isTumor = true;
             var message = new OSCMessage("/message/address");
             message.AddValue(OSCValue.String("isTumor"));
             transmitter.Send(message); 
-        }
-    }
-
-    protected void MessageReceived(OSCMessage message)
-    {
-        Debug.Log("message sent! " + message);
+        } 
     }
 }
--- Model/Assets/VesselSound.cs
+++ Model/Assets/VesselSound.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using extOSC;
 
 public class VesselSound : MonoBehaviour
 {
@@ -9,11 +10,13 @@ public class VesselSound : MonoBehaviour
     public MeshCollider vesselSurface;
     private AudioSource audioSource;
     private float distance;
+    private OSCTransmitter transmitter;
+
     void Start()
     {
         transmitter = gameObject.AddComponent<OSCTransmitter>();
         transmitter.RemoteHost = "131.159.205.151";    
-        transmitter.RemotePort = 8001;  
+        transmitter.RemotePort = 7001;  
         InvokeRepeating("Vessel", (float) 0.3, (float) 0.3);
     }
 
@@ -28,17 +31,24 @@ public class VesselSound : MonoBehaviour
     {
         float current_dist = distance * 100.0f;
         if (current_dist <= 1.5f && current_dist > 1.0f) {
+            print("vessel3");
             var message = new OSCMessage("/message/address");
             message.AddValue(OSCValue.String("vessel3"));
             transmitter.Send(message); 
         } else if (current_dist <=1.0f && current_dist > 0.5f) {
+            print("vessel2");
             var message = new OSCMessage("/message/address");
             message.AddValue(OSCValue.String("vessel2"));
             transmitter.Send(message); 
         } else if (current_dist <=  0.5f && current_dist >= 0.0f) {
+            print("vessel1");
             var message = new OSCMessage("/message/address");
             message.AddValue(OSCValue.String("vessel1"));
             transmitter.Send(message); 
+        } else {
+            var message = new OSCMessage("/message/address");
+            message.AddValue(OSCValue.String("bequiet"));
+            transmitter.Send(message);
         }
     }
 }
